719c38c7a45c210cb7d0beb57c1a540772061c31
Victor-Leroy <victor.leroy@algosup.com>
2024-01-16 10:00:11 +0100

    fileAdd

---
+++ backEnd/fileAdd.c
@@ -0,0 +1,18 @@
+// product ID 
+// name
+// qty 
+// price
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    FILE *fptr;
+
+    // Create a file
+    fptr = fopen("test.txt", "w");
+
+    // Close the file
+    fclose(fptr);
+}
